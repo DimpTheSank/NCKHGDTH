4e04c070fd1c105d7116bb3f1f77859885fec56b
DimpTheSank <dangnlh.auto@gmail.com>
2026-09-15 13:39:33 +0700

    Reset app metadata

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'EduGame - Học tập vui vẻ',
-  description: 'Nền tảng học tập gamification cho học sinh tiểu học',
+  title: 'Game',
+  description: 'Trang trò chơi',
 }
 
 export default function RootLayout({ children }) {
