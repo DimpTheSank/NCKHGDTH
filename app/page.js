4a7b52ff9c364e58a5bbbcbe4eac1120a867df2c
DimpTheSank <dangnlh.auto@gmail.com>
2026-09-24 19:02:50 +0700

    Restrict student home to student role

--- app/page.js
+++ app/page.js
@@ -166,5 +166,5 @@ function StudentHome() {
 }
 
 export default function HomePage() {
-  return <ProtectedRoute><StudentHome /></ProtectedRoute>;
+  return <ProtectedRoute allowedRoles={["student"]}><StudentHome /></ProtectedRoute>;
 }
